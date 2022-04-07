b90de6788ff773250dfc2dd94fa0b3432eee67b5
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-07 05:58:38 -0700

    first try

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (void)
+{ 
+/*writing a program to print
+ *programming is like building a multilingual puzzle
+ */
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
