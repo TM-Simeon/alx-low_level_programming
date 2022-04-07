f882c7f707e42c4c1e8e3a8efadf1dc4edc786ac
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-07 06:04:05 -0700

    third try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main (void)
 { 
-/*writing a program to print
+/**
+ * writing a program to print
  *programming is like building a multilingual puzzle
  */
 	puts("\"Programming is like building a multilingual puzzle");
