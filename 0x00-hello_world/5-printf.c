72b016076f673d0c31e7c34ad367ca554b2d0573
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-07 06:58:58 -0700

    first try

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ * main - A program that prints \" with proper grammar, but ..
+ *
+ * Return: return zero if the program works correctly
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
