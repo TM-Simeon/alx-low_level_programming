0bac25823dd17cc304ee3337d40b18931215557e
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-08 13:11:28 -0700

    first try

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - different combination of three digits
+ *
+ * Return: 0 is always success
+ */
+int main(void)
+{
+	int n, m, x;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (x = 50; x <58; x++)
+			{
+				if (x > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(x);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
