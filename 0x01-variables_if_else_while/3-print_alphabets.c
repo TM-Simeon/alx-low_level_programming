12d54917a0ac6a65ec92e72187bae445e14f752b
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-08 10:51:00 -0700

    first try

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - lower and upper case alphabet
+ *
+ * Return: 0 is always success
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++);
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++);
+	{
+		putchar(ch);
+	}
+	putchar('\n);
+	return (0);
+}
