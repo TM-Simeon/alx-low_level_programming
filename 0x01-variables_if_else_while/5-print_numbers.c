376a9d691a3a93eb0bd599e86976778859b65ca5
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-08 11:27:37 -0700

    first try

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - all single digits of base 10
+ *
+ * Return: 0 is always success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf(num);
+	}
+	printf("\n");
+	return (0);
+}
+
