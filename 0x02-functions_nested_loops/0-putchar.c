d34a9346c3606ff188e92e579e3a7655405da0d1
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-12 10:08:17 -0700

    code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	char p[8] = {'_' , 'p' , 'u' , 't' , 'c' , 'h' , 'a' , 'r'};
+	char p[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	int i;
 	for (i = 0; 1 < 8; i++)
 	{
 		putchar(p[i]);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
