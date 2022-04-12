c8b4534d672dc62689046df943f2a9727414fa21
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-12 09:06:56 -0700

    new commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,6 @@
-#include <unistd.h>
-#include "main.h"
-/**
- * _putchar - print putchar 
- * @c: the character to print
- *
- * Return: 1 for success
- * on error, -1 is returned
- */
-int _putchar(char c)
+#include <stdio.h>
+int main(void)
 {
-	return (write(1, &c, 1));
+	putchar("_putchar\n");
+	return 0;
 }
