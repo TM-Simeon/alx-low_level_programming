31d37dd6f837abd7d51e4f4cbf7e284343e51647
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-12 10:00:06 -0700

    helped

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,12 @@
  */
 int main(void)
 {
-	_putchar("_";
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-
+	char p[8] = {'_','p','u','t','c','h','a','r'};
+	int i;
+	for (i=0; 1 < 8; i++)
+	{
+		putchar(p[i]);
+	}
+	putchar('\n');
 	return(0);
 }
