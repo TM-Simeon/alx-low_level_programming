9946a8431c22278a8e7d33ec5bdd59e6fc584fe6
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-13 08:48:15 -0700

    first try

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - search for alpha
+ * @c : character to find
+ * return: 0 always
+ */
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >= 65 && c >=90));
+}
