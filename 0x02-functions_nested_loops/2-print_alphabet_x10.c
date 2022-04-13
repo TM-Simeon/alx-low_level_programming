594ce72137c94afc1bb51e55c2d10f3dbaa3fcde
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-13 08:10:39 -0700

    ung

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main -print 10 times the alphabet
+ *
+ * return: 0 always
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		char c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
