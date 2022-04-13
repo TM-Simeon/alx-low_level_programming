c260332b1cc6348b6ca88e3822cc15761f55eda8
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-13 13:45:50 -0700

    try

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * abs - find the absolute value
+ * @n : the integer to use
+ *
+ * return: 0 always
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
