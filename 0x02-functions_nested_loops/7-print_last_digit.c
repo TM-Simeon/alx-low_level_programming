428b07346a7578f5d2682faf67d299d1fe2da820
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-13 14:12:45 -0700

    try

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @n : the integer to use
+ *
+ * Return: 0 always
+ */
+int print_last_digit(int n)
+{
+	int i;
+
+	i = n % 10;
+	_putchar(i);
+	return (i)
+}
