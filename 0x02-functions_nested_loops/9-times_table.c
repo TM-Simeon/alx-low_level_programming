ed24db9e063fe8b7a750b67ebecaaea3552fa117
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-13 15:33:22 -0700

    try

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * times_table - print times table
+ *
+ * Return: 0 always
+ */
+void time_table(void)
+{
+	int a = 0;
+	int b;
+	int rep;
+	
+	while (a <= 9)
+	{
+		rep = a * b;
+		if (b == 0)
+		{
+			_putchar('0' + rep);
+		}
+		else if (rep < 10)
+		{
+			_putchar(' ');
+			_putchar('0' + rep);
+		}
+		else
+		{
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+		if (b < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		b++;
+	}
+	_putchar('\n');
+	a++;
+}
