4248b7ed17c80b1d6ab6454300e6c768f4d87452
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-14 01:51:25 -0700

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
+
 /**
- * positive_or_negative - check the sign of a number
- *
- * Return: 0 always
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
