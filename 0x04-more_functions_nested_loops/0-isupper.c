55c04e7ea4f82ff64e6d22c90a488f70df91e921
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-14 13:04:39 -0700

    try

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper case
+ * @c: character to take in
+ *
+ * Return: 0 always
+ */
+int _isupper(int c)
+{
+	if (c = 'A'; c <= 'Z'; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
