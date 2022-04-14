72ee7c3533d659cd9b4ca91d82c9f31600d8b253
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-13 18:07:42 -0700

    try

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,13 +6,38 @@
  */
 void times_table(void)
 {
-	int a, b, c;
-
-	for (a = 0; a <= 9; a++)
+	int a = 0;
+	int b;
+	int rep;
+	
+	while (a <= 9)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
+		b = 0;
+		while (b <= 9)
+		{
+			rep = a * b;
+			if (b == 0)
+			{
+				_putchar('0' + rep);
+			}
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else
+			{
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			b++;
+		}
+		_putchar('\n');
+		a++;
 	}
-
 }
---
+++ 0x02-functions_nested_loops/x-file.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * times_table - print times table
+ *
+ * Return: 0 always
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+	}
+
+}
