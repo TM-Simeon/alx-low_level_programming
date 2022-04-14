540ca25e7be19efedd2159a216288b8b207ed403
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-13 22:24:39 -0700

    try

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * positive_or_negative - check the sign of a number
+ * @n : the integer returned
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+{
+
