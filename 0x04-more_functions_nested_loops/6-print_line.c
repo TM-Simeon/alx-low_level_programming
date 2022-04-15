0db337729b6866c2dfd9609abe00f37d94f01bcc
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-15 06:03:39 -0700

    try

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draw straight line
+ *
+ * Return: 0 always
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
