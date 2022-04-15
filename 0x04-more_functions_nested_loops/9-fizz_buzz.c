f5a0ca384d55f974c407e7379a44ab8012c08ecc
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-15 07:04:48 -0700

    try

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - interview question design
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 3 != 0 && i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 %% i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		printf("%d ", i);
+	}
+	printf("\n");
+}
