e8abef683e21fffc8a9e40edbe2ebf0d857c3cab
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-20 16:34:34 -0700

    update

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int numb;
+	srand(time(0));
+	num = rand();
+	printf("%i\n", numb);
+	return (0);
+}
