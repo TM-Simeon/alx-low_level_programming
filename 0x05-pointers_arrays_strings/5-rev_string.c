3837d3714b79c25e5ce04eb3d744e8c9dafcf90a
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-20 13:29:21 -0700

    update

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - function to call
+ * @s: parameter to take in
+ *
+ * Return: always 0
+ */
+void rev_string(char *s)
+{
+	int i, j, temp;
+	int c;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		c++
+	}
+	for (i = 0, j = c - 1; i != j; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
