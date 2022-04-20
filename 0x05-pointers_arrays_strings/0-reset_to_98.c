6f86db0aaab443c5ebbb52f36da9fa4c4680927f
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-20 08:10:19 -0700

    now

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,20 +9,3 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
-/**
- * main -entry point
- *
- * Return: 0 always
- *
-int main(void)
-{
-	int a;
-	int *p;
-
-	p = &a;
-	a = 400;
-	reset_to_98(p);
-	_putchar(a);
-
-	return (0);
-}*/
