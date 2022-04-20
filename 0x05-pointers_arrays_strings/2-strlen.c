bf626a7261b1f2e53905cc80ddd65741d80b27e4
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-20 11:29:02 -0700

    first try

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - called function
+ * @s: passed parameter
+ *
+ * Return: always 0
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while(*s != '\0')
+	{
+		c++;
+		s++;
+	}
+	_putchar(c);
+}
