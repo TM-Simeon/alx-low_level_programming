c66e09160d6d2fc680c56855b90c926174b07a75
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-20 07:40:45 -0700

    first try

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * reset_to_98 - change the value of a number
+ * @*n: takes in a pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	n = 98;
+}
+/**
+ * main -entry point
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a;
+	int *p;
+
+	p = &a;
+	a = 400;
+	reset_to_98(p);
+	_putchar(a);
+
+	return (0);
+}
