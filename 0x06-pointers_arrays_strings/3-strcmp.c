f5fc5f85177841bbec99ddc26400c934dff1fec9
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-22 12:59:38 -0700

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcmp - called function
+ * @s1: first parameter
+ * @s2: second parameter
+ *
+ * Return: 0 always
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = strcmp(s1, s2);
+	return (a);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_INCLUDE
+#define MAIN_H_INCLUDE
+
+void _putchar(char ch);
+char *_strcat(char *dest, char *src);
+char *strncat(char *dest, char *src, int n);
+char *strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+
+#endif
