756863d5af7339f23765c04ee151f63e95bd2fea
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-23 13:29:47 -0700

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - called function
+ * @s1: parameter one
+ * @s2: parameter two
+ *
+ * Return: always 0
+ */
+int -strcmp(char *s1, char *s2)
+{
+	char *str_one = s1;
+	char *str_two = s2;
+
+	while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+	{
+		str_one++;
+		str_two++;
+	}
+	return (*str_one - *str_two);
+}
