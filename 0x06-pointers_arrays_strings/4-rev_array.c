d0f83a04bb5747866ff55c59066845b18145285c
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-23 13:38:06 -0700

    update

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * reverse_array - called function
+ * @a: passed in value one
+ * @n: passed in value two
+ *
+ * Return: returns void
+ */
+void reverse_array(int *a, int n)
+{
+	int *start_c, *end_c, c;
+	int i;
+
+	start_c = a;
+	end_c = a;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		end_c++;
+	}
+
+	for (i = 0; i < n / 2; i++)
+	{
+		c = *end_c;
+		*end_c = *start_c;
+		*start_c = c;
+
+		start_c++;
+		end_c--;
+	}
+}
