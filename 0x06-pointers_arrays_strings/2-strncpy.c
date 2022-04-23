2dac29a08b156596f2d1fd3688e121e0b9f74d91
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-23 12:23:29 -0700

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,16 @@ car *_strncpy(char *dest, char *src, int n);
 
 	i = 0;
 	j = 0;
-	while (i < n)
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
+	}
+	while ( i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
