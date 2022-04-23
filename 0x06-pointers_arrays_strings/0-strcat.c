03de288699a60190a2a4fe9b3506260b9152573f
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-23 05:47:13 -0700

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,6 +8,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
-	return (dest);
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return dest;
 }
