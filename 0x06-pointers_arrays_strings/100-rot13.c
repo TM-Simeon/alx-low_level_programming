f3cb7f8a0c4bff6fb56f7c9fa16a3b6b987033ef
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-24 15:35:49 -0700

    update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,6 +5,7 @@
  *
  * Return: the pointer to dest
  */
+
 char *rot13(char *s)
 {
 	int count = 0, i;
@@ -13,7 +14,7 @@ char *rot13(char *s)
 	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	while (*(s + count) != '\0')
 	{
-		for (i = 0; i <= 52; i++)
+		for (i = 0; i < 52; i++)
 		{
 			if (*(s + count) == alphabet[i])
 			{
