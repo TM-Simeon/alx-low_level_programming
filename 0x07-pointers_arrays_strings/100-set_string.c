a940d1381c7f33a079e2b500e6874a7799cd0932
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-26 09:31:11 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - function to call
+ * @s: pointer to set
+ * @to: destination pointer
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
