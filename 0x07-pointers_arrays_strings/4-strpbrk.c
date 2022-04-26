766d09ed0d735ba0553a29cf6cca725b5445a88f
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-26 08:52:44 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk - sesarch a string for a set of bytes
+ * @s: passed in string
+ * @accept: bytes to check
+ * Return: destination string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	while (*s != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+			j++;
+		}
+		s++;
+	}
+	return (0);
+}
