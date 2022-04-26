6ca269b0c263a59a34b86e0faa2265d1a4f4b1a4
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-04-26 05:49:02 -0700

    update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
+void _putchar(char ch);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchar(char *s, char c);
