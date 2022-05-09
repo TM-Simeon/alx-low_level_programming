02cf33b92e34b9289cd2c9430910c549022763e8
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-05-09 11:37:58 -0700

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 
-	while (n < len2 && i < (len1 _ n))
+	while (n < len2 && i < (len1 + n))
 		s[i++] = s2[j++];
 
 	while (n >= len2 && i < (len1 + len2))
