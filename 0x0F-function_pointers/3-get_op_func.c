cac22f9759a8f7ddc80677d5dcaa294007187d57
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2022-05-14 08:08:22 -0700

    update

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * get_op_func - compares function with pointer and returns the equal
+ * @s: pointers a char
+ * Return: returns the function and the operator when equal
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (!strcmp(ops[i].op, s))
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
