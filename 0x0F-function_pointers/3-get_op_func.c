38d86169b3c61eccc0edfcaa4765f6a037379ea6
TM-Simeon <99520993+TM-Simeon@users.noreply.github.com>
2022-05-14 16:12:13 +0100

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,28 +3,29 @@
 #include <string.h>
 
 /**
- * get_op_func - compares function with pointer and returns the equal
- * @s: pointers a char
- * Return: returns the function and the operator when equal
- */
+ * get_op_func - Compares function with pointer and returns the equal.
+ * @s: points a char
+ * Return: Returns the function and the operator when equal
+**/
 
 int (*get_op_func(char *s))(int, int)
 {
-	op_t[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
 
-	while (ops[i].op != NULL)
-	{
-		if (!strcmp(ops[i].op, s))
-			return (ops[i].f);
-		i++;
-	}
-	return (NULL);
+int i = 0;
+
+while (ops[i].op != NULL)
+{
+if (!strcmp(ops[i].op, s))
+return (ops[i].f);
+i++;
+}
+return (NULL);
 }
